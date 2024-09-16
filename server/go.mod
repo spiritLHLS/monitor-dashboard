871d80e765b48eb4f2e2b1d86949ef2ccc026d19
spiritlhl <admin@spiritlhl.net>
2024-09-16 19:33:18 +0800

    Update

--- server/go.mod
+++ server/go.mod
@@ -1,6 +1,8 @@
 module github.com/flipped-aurora/gin-vue-admin/server
 
-go 1.22
+go 1.23
+
+toolchain go1.23.1
 
 require (
 	github.com/aliyun/aliyun-oss-go-sdk v2.2.7+incompatible
@@ -40,9 +42,9 @@ require (
 	go.mongodb.org/mongo-driver v1.12.1
 	go.uber.org/automaxprocs v1.5.3
 	go.uber.org/zap v1.24.0
-	golang.org/x/crypto v0.22.0
-	golang.org/x/sync v0.6.0
-	golang.org/x/text v0.14.0
+	golang.org/x/crypto v0.27.0
+	golang.org/x/sync v0.8.0
+	golang.org/x/text v0.18.0
 	gorm.io/datatypes v1.2.1
 	gorm.io/driver/mysql v1.5.6
 	gorm.io/driver/postgres v1.5.7
@@ -55,7 +57,9 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/PuerkitoBio/goquery v1.10.0 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/bodgit/plumbing v1.2.0 // indirect
 	github.com/bodgit/sevenzip v1.3.0 // indirect
 	github.com/bodgit/windows v1.0.0 // indirect
@@ -146,11 +150,11 @@ require (
 	go4.org v0.0.0-20200411211856-f5505b9728dd // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/image v0.15.0 // indirect
-	golang.org/x/mod v0.14.0 // indirect
-	golang.org/x/net v0.21.0 // indirect
-	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/time v0.1.0 // indirect
-	golang.org/x/tools v0.17.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/telebot.v3 v3.3.8 // indirect
--- server/initialize/plugin.go
+++ server/initialize/plugin.go
@@ -2,17 +2,20 @@ package initialize
 
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
+	"github.com/flipped-aurora/gin-vue-admin/server/plugin/allpdspiders"
 	"github.com/flipped-aurora/gin-vue-admin/server/plugin/client"
+	"github.com/flipped-aurora/gin-vue-admin/server/plugin/spiders"
 	"github.com/flipped-aurora/gin-vue-admin/server/plugin/telegram_bot"
+	"github.com/flipped-aurora/gin-vue-admin/server/plugin/telegrampush"
 	"github.com/gin-gonic/gin"
 )
 
-func InstallPlugin(PrivateGroup *gin.RouterGroup, PublicRouter *gin.RouterGroup, engine *gin.Engine) {
+func InstallPlugin(PrivateGroup *gin.RouterGroup, PublicGroup *gin.RouterGroup, engine *gin.Engine) {
 	if global.GVA_DB == nil {
 		global.GVA_LOG.Info("项目暂未初始化，无法安装插件，初始化后重启项目即可完成插件安装")
 		return
 	}
-	bizPluginV1(PrivateGroup, PublicRouter)
+	bizPluginV1(PrivateGroup, PublicGroup)
 	bizPluginV2(engine)
 	// 注册TG发信插件
 	PluginInit(PrivateGroup, telegram_bot.CreateTelegram_botPlug())
@@ -22,5 +25,11 @@ func InstallPlugin(PrivateGroup *gin.RouterGroup, PublicRouter *gin.RouterGroup,
 	tgbotToken := global.GVA_VP.GetString("tgr.tgbot_token")
 	codeLength := global.GVA_VP.GetInt("tgr.verification_code_length")
 	channelChatID := global.GVA_VP.GetString("tgr.channel_chat_id")
-	PluginInit(PublicRouter, client.CreateRegisterPlug(uint(authorityID), tgbotToken, codeLength, channelChatID, tgrAdmins))
+	PluginInit(PublicGroup, client.CreateRegisterPlug(uint(authorityID), tgbotToken, codeLength, channelChatID, tgrAdmins))
+	// Telegram频道信息推送插件注册
+	PluginInit(PrivateGroup, telegrampush.CreateTelegramPushPlug())
+	// 多节点爬虫插件注册
+	PluginInit(PublicGroup, spiders.CreateSpidersPlug())
+	// 商家所有商品爬虫推送信息插件注册
+	PluginInit(PublicGroup, allpdspiders.CreateAllPdSpidersPlug())
 }
