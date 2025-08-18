ea37c525e8c7091aace3799a0d4d919d4f83c1e7
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2025-08-18 11:06:54 +0800

    fix

--- server/model/digitalproducts/digitalproducts.go
+++ server/model/digitalproducts/digitalproducts.go
@@ -13,10 +13,10 @@ type DigitalProducts struct {
 	Disk       *float64 `json:"disk" form:"disk" gorm:"column:disk;comment:以GB计算的大小;"`                     //硬盘
 	Traffic    *float64 `json:"traffic" form:"traffic" gorm:"column:traffic;comment:以T计算的大小;"`             //流量
 	PortSpeed  *float64 `json:"portSpeed" form:"portSpeed" gorm:"column:port_speed;comment:以Gbps计算的大小;"`   //带宽
-	Location   *string  `json:"location" form:"location" gorm:"column:location;comment:;size:191;"`        //位置
+	Location   *string  `json:"location" form:"location" gorm:"column:location;comment:;size:200;"`        //位置
 	Price      *float64 `json:"price" form:"price" gorm:"column:price;comment:价格大小;"`                      //价格
 	PriceUnit  *string  `json:"priceUnit" form:"priceUnit" gorm:"column:price_unit;comment:价格单位;size:20;"` //价格单位
-	Additional *string  `json:"additional" form:"additional" gorm:"column:additional;comment:;size:191;"`  //其他
+	Additional *string  `json:"additional" form:"additional" gorm:"column:additional;comment:;size:350;"`  //其他
 	OriginId   *int     `json:"originId" form:"originId" gorm:"column:origin_id;comment:原表主键ID;size:20;"`  //原表ID
 	CreatedBy  uint     `gorm:"column:created_by;comment:创建者"`
 	UpdatedBy  uint     `gorm:"column:updated_by;comment:更新者"`
